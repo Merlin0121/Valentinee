e0899b5da172e950719d651b9f38954c90bc1cf1
Merlin0121 <tm0121911@gmail.com>
2026-02-03 13:26:09 +0700

    Update secret code and improve event handling

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Ánh'; // Replace with your secret code
+const secretCode = 'baby'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
@@ -33,4 +33,5 @@ form.addEventListener('submit', (e) => {
             confirmButtonText: 'Retry'
         });
     }
+
 });
